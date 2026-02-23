a3ad8e5f664228c1f8fefa404a0d82808f740903
Prajakta Bhujabl <prajakta.bhujbal@sumasoft.net>
2026-02-23 23:04:22 +0530

    Initial commit

--- Chapter_01_Basics/03_Verify_Setup.js
+++ Chapter_01_Basics/03_Verify_Setup.js
@@ -1,1 +1,11 @@
 console.log(process.platform)
+//Mac OS - darwin
+//Windows - win32
+//Linux - linux
+
+console.log(process.arch);   //Architecture - how may bits you are using
+//x64
+//arm64 
+//ia32
+
+console.log(process.version);
