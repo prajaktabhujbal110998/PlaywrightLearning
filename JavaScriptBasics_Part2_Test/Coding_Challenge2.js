17b00fa8256a9a941ebbd220b788e0b95516dd67
Pr@jakta1198 <prajakta.bhujbal02@gmail.com>
2026-03-30 12:46:05 +0530

    Update Coding_Challenge2.js

--- JavaScriptBasics_Part2_Test/Coding_Challenge2.js
+++ JavaScriptBasics_Part2_Test/Coding_Challenge2.js
@@ -1,7 +1,7 @@
 //After a test suite runs, you receive an array of test results (strings: "pass", "fail", "skip"). Write a JavaScript program using a for loop that counts how many tests passed, failed, and were skipped. Print a test report with total tests, counts, pass rate percentage, and a verdict (all passed → ready for release, ≤2 failures → review, >2 failures → block release).
 
 
-let testResults = ["pass", "fail", "pass", "skip", "fail", "pass", "pass", "fail", "skip"];
+let testResults = ["pass", "pass", "fail", "pass", "skip", "pass", "fail", "pass"]
 
 let passCount = 0;
 let failCount = 0;
